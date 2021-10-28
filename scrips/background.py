c698db3fec4acac217548197a30fae4afa1e896d
Phuc_Perseids <hiennhanomoris2001@gmail.com>
2021-10-28 15:50:37 +0700

    ham su ly cac van de background

--- scrips/background.py
+++ scrips/background.py
@@ -2,6 +2,7 @@ import pygame
 import game_controller
 import objects
 
+# cac thuoc tinh, chuc nang background
 class Backgroud(objects.Object):
     def __init__(self, x_pos, y_pos, image, x_vel, y_vel):
-        super().__init__(x_pos, y_pos, image, x_vel, y_vel)
+        super().__init__(x_pos, y_pos, image, x_vel, y_vel) 

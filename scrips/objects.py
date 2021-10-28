63275035cbc8d858fdcff68e2da8a39fce492cbf
Phuc_Perseids <hiennhanomoris2001@gmail.com>
2021-10-28 15:52:46 +0700

    obj cha

--- scrips/objects.py
+++ scrips/objects.py
@@ -1,9 +1,10 @@
 import pygame
 
+# thuoc tinh chung cua cac vat the
 class Object:
     def __init__(self, x_pos, y_pos, image, x_vel, y_vel):
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.image = pygame.image.load(image)
-        self.x_vel = x_vel
-        self.y_vel = y_vel
+        self.x_pos = x_pos                         #toa do Ox
+        self.y_pos = y_pos                         #toa do Oy
+        self.image = pygame.image.load(image)      #hinh anh
+        self.x_vel = x_vel                         #toc do theo chieu Ox (neu co)
+        self.y_vel = y_vel                         #toc do theo chieu Oy (neu co)

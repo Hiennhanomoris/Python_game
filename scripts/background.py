ec0883d70283f20df3dd86bd2e1e51bbcf8a4a16
Phuc_Perseids <hiennhanomoris2001@gmail.com>
2021-10-28 21:19:40 +0700

    class background

--- scripts/background.py
+++ scripts/background.py
@@ -6,3 +6,5 @@ import objects
 class Backgroud(objects.Objects):
     def __init__(self, x_pos, y_pos, image, x_vel, y_vel):
         super().__init__(x_pos, y_pos, image, x_vel, y_vel) 
+
+        

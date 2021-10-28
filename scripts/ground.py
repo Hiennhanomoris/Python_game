8e2e49ca498a00e1e99465464b1943430f321420
Phuc_Perseids <hiennhanomoris2001@gmail.com>
2021-10-28 21:22:17 +0700

    class ground

---
+++ scripts/ground.py
@@ -0,0 +1,6 @@
+import pygame
+import objects
+
+class Ground(objects.Objects):
+    def __init__(self, x_pos, y_pos, image, x_vel, y_vel):
+        super().__init__(x_pos, y_pos, image, x_vel, y_vel)

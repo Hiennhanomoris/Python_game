1d2f31f222e7158080be2c0c270795576e6fb389
Phuc_Perseids <hiennhanomoris2001@gmail.com>
2021-11-06 22:38:06 +0700

    add play again function

--- scripts/game_controller.py
+++ scripts/game_controller.py
@@ -15,7 +15,8 @@ pygame.display.set_caption("RUN NOW")
 bg1 = background.Background(0,0,"images/bg.jpg",0,0)
 playerr = player.Player(250, 90, "images/player.png", 0, 0, mixer)
 score = textx.Textx("consolas", 30, "Score", (100, 200, 168))
-game_over = textx.Textx("consolas", 60, "Score", (255, 30, 0))
+game_over = textx.Textx("consolas", 60, "game over", (255, 30, 0))
+play_again = textx.Textx("consolas", 20, "play again", (255, 255, 255))
 landstart = land.Land(0, 250,"images/LAND/landstart.png",0,0, 130, 40)
 land1 = land.Land(200, 240,"images/LAND/land1.png",0,0, 120, 45)
 land2 = land.Land(400 ,250,"images/LAND/land2.png",0,0, 120, 40)
@@ -41,6 +42,19 @@ def movement():
     for land in land_group:
         land.update()
 
+def reset():
+    playerr.die = False
+    score.point = 0
+    playerr.rect.y = 90
+    land1.rect.x = 200
+    land1.rect.y = 240
+    land2.rect.x = 400
+    land2.rect.y = 250
+    land3.rect.x = 600
+    land3.rect.y = 260
+    landstart.rect.x = 0
+    landstart.rect.y = 250
+
 #vong lap game
 while quit_game == False:
     clock.tick(fps)
@@ -50,7 +64,11 @@ while quit_game == False:
         hien_thi()
         movement()
     else:
-        game_over.hien_thi(screen, 250, 200, "GAME OVER!!!")
+        game_over.hien_thi(screen, 230, 200, "GAME OVER!!!")
+        play_again.hien_thi(screen, 260, 30, "press space to try again :)")
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE]:
+            reset()
 
     for event in pygame.event.get():
         #Quit game

8fcac99cfb65523b3af8735e2b87052598c115ba
Phuc_Perseids <hiennhanomoris2001@gmail.com>
2021-11-06 20:29:37 +0700

    thay doi do cao khi sinh land

--- scripts/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-    # if land1.x_pos < -land1.width:
-    #     land1.x_pos = 800 - land1.width
-    #     land1.y_pos = random.randint(235,265)
-    # if land2.x_pos < -land2.width:
-    #     land2.x_pos = 800 - land2.width
-    #     land2.y_pos = random.randint(235,265)
-    # if land3.x_pos < -land3.width:
-    #     land3.x_pos = 800 - land3.width
-    #     land3.y_pos = random.randint(235,265)

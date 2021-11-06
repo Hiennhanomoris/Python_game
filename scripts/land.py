14adc579ce07d439d4dc17d4a0305a4d7434e7ed
Phuc_Perseids <hiennhanomoris2001@gmail.com>
2021-11-06 22:02:37 +0700

    complete land

--- scripts/land.py
+++ scripts/land.py
@@ -5,14 +5,9 @@ import objects
 class Land(objects.Objects):
     def __init__(self, x_pos, y_pos, image, x_vel, y_vel, width, height):
         super().__init__(x_pos, y_pos, image, x_vel, y_vel)
-    # def __init__(self,x_pos, y_pos, width, height, image, x_vel, y_vel):
-    #     self.x_pos = x_pos                         
-    #     self.y_pos = y_pos                             
-    #     self.x_vel = x_vel                         
-    #     self.y_vel = y_vel     
         self.width = width
         self.height = height  
-        self.land_speed = 2 #toc do di chuyen cuc dat di dong
+        self.land_speed = 2                 #toc do di chuyen cuc dat di dong
         img = pygame.image.load(image)  
         self.image = pygame.transform.scale(img, (self.width, self.height)) 
         self.rect = self.image.get_rect()
